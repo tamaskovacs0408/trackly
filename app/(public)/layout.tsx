684c94741ac7d1c427a7f25518ae3f433045e4a3
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-11-12 07:59:15 +0100

    Add landing layout

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+export default function Layout({ children }: { children: React. ReactNode}) {
+  return (
+    <section className="flex min-h-screen flex-col max-w-7xl mx-auto">
+      <Header />
+      <main className="flex-1 pb-20">{children}</main>
+      <Footer />
+    </section>
+  )
+}
