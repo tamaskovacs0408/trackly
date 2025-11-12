bf189ef9c8ca77cb6d4a4dd8acd25d52fcf9033e
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-11-12 12:22:54 +0100

    Add responsive breakpoint

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/layout/Footer";
 
 export default function Layout({ children }: { children: React. ReactNode}) {
   return (
-    <section className="flex min-h-screen flex-col max-w-7xl mx-auto">
+    <section className="flex min-h-screen flex-col max-w-7xl xl:max-w-3xl mx-auto">
       <Header />
       <main className="flex-1 pb-20">{children}</main>
       <Footer />
