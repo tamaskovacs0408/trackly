19a0d8512471b095b09522471c4a6f2da21e1524
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-11-12 12:23:08 +0100

    Add component to landing page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,13 +1,18 @@
 import HeroSection from "@/components/landing/HeroSection";
+import TopAlbums from "@/components/landing/TopAlbums";
+
+import { shuffleArray } from "@/lib/utils";
 
 import { albums } from "@/lib/faker-data";
 
 export default function Home() {
   const featuredAlbum = albums[0];
+  const topAlbums = shuffleArray(albums.slice(1)).slice(0, 9);
 
   return (
     <section className="container mx-auto">
       <HeroSection album={featuredAlbum} />
+      <TopAlbums albums={topAlbums} />
     </section>
   )
 }
